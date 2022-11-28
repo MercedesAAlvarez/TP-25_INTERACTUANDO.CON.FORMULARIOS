0d2c1d69d7dadeb170fb590bd6e01988ef1b5031
MercedesAAlvarez <mechychevy@gmail.com>
2022-11-28 18:23:12 -0300

    en proceso tp 25

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function () {
     let titulo = document.querySelector('.moviesAddTitulo')
     let formulario = document.querySelector('#formulario');
     let article = document.querySelector('article');
@@ -7,50 +7,53 @@ window.onload = function(){
     article.classList.add('fondoTransparente');
     formulario.classList.add('fondoCRUD');
 
-//------DESDE AQUÍ CONTINÚE CON LAS VALIDACIONES DEL FORMULARIO //
-//-------------------DE REGISTRO DE PELÍCULAS------------------//    
- 
-console.log('archivos vinculados exitosamente')
+    //------DESDE AQUÍ CONTINÚE CON LAS VALIDACIONES DEL FORMULARIO //
+    //-------------------DE REGISTRO DE PELÍCULAS------------------//    
 
-let $ = (e) => document.querySelector(e)
+    console.log('archivos vinculados exitosamente')
 
-const funcValidate = (obj) => {
-    let arr = Object.values(obj)
-    if(!arr.includes(false)){
-        btn.disabled= false
-        btn.style.backgroundColor = '#198754' 
-    }else{
-        btn.disabled = true
-        btn.style.backgroundColor = '#7b9c8d'
+    let $ = (e) => document.querySelector(e)
+
+    const funcValidate = (obj) => {
+        let arr = Object.values(obj)
+        if (!arr.includes(false)) {
+            btn.disabled = false
+            btn.style.backgroundColor = '#198754'
+        } else {
+            btn.disabled = true
+            btn.style.backgroundColor = '#7b9c8d'
+        }
     }
-}
 
 
-let inputTitulo = $('#title')
-let rating = $('#rating')
-let awards = $('#awards')
-let release_date = $('#release_date')
-let duracion = $('#length')
-let genre_id = $('#genre_id')
-let btn = $('#btn-agregar')
+    let inputTitulo = $('#title')
+    let rating = $('#rating')
+    let awards = $('#awards')
+    let release_date = $('#release_date')
+    let duracion = $('#length')
+    let genre_id = $('#genre_id')
+    let btn = $('#btn-agregar')
 
 
 
 
-inputTitulo.focus()
+    inputTitulo.focus()
 
-inputTitulo.addEventListener('blur',(e) =>{
+    inputTitulo.addEventListener('blur', (e) => {
+        if (!inputTitulo.value) {
+            inputTitulo.classList.add('is-invalid')
+        }
+        console.log(inputTitulo);
+    })
 
-})
 
+    const validate = {
+        titutlo: false,
+        rating: false,
+        release: false,
+        duracion: false,
+        genre: false
 
-const validate = {
-    titutlo : false,
-    rating : false,
-    release : false,
-    duracion: false,
-    genre : false
-
-}
-funcValidate(validate)
+    }
+    funcValidate(validate)
 }
