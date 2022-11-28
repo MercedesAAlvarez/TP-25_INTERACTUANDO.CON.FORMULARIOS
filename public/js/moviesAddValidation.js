7ca0e4387c495405d431e414cba3628d25861e32
MercedesAAlvarez <mechychevy@gmail.com>
2022-11-28 15:51:26 -0300

    en proceso tp 25

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -14,10 +14,27 @@ console.log('archivos vinculados exitosamente')
 
 let $ = (e) => document.querySelector(e)
 
+
 let inputTitulo = $('#title')
+let rating = $('#rating')
+let awards = $('#awards')
+let release_date = $('#release_date')
+let duracion = $('#length')
+let genre_id = $('#genre_id')
+let btn = $('#btn-agregar')
+
+
+
 
 inputTitulo.focus()
 
 
+const validate = {
+    titutlo : false,
+    rating : false,
+    release : false,
+    duracion: false,
+    genre : false
 
+}
 }
