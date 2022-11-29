7164e417da93c9decf400688b4bb90fc8e5a66c9
MercedesAAlvarez <mechychevy@gmail.com>
2022-11-29 18:02:25 -0300

    tp 25 finalizado

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -26,51 +26,74 @@ window.onload = function () {
     }
     /* 'btn' boton agregar/enviar si los campos estan vacios no me permite enviar el formulario */
 
+    let form = $('#form')
+    let errores = $('#errores')
 
     let inputTitulo = $('#title')
     let rating = $('#rating')
     let awards = $('#awards')
-    let release= $('#release_date')
+    let release = $('#release_date')
     let duracion = $('#length')
     let genre = $('#genre_id')
+
+
     let btn = $('#btn-agregar')
 
-
-
-
     inputTitulo.focus()
+
 
     inputTitulo.addEventListener('blur', (e) => {
         if (!inputTitulo.value) {
             inputTitulo.classList.add('is-invalid')
             inputTitulo.classList.remove('is-valid')
+            errores.classList.add('alert-warning')
+            errores.innerHTML = error
             validate.titulo = false
-        }else{
+        } else {
             inputTitulo.classList.remove('is-invalid')
             inputTitulo.classList.add('is-valid')
+            $('#titleError').innerHTML = ""
+            errores.classList.remove('alert-warning')
             validate.titulo = true
         }
         /* console.log(inputTitulo); */
         funcValidate(validate)
     })
     rating.addEventListener('blur', (e) => {
+
         if (!rating.value) {
+            let error = "<li id='ratingError'>Debes escribir una puntuacion</li>"
             rating.classList.add('is-invalid')
             rating.classList.remove('is-valid')
+            errores.classList.add('alert-warning')
+            $('#ratingError') ? null : errores.innerHTML += error
             validate.rating = false
-        }else{
+        } else if (rating.value <= 0 || rating.value > 10) {
+            rating.classList.add('is-invalid')
+            rating.classList.remove('is-valid')
+            errores.classList.add('alert-warning')
+            $('#ratingError')?  $('#ratingError').innerText = "Debe ser entre 1 y 10" : errores.innerHTML += error
+            
+            validate.rating = false
+            
+        } else {
             rating.classList.remove('is-invalid')
             rating.classList.add('is-valid')
+           
+            errores.removeChild( $('#ratingError'))
             validate.rating = true
         }
         funcValidate(validate)
     })
     awards.addEventListener('blur', (e) => {
+        let error = "<li id='titleError'>Debes escribir un titulo</li>"
         if (!awards.value) {
             awards.classList.add('is-invalid')
             awards.classList.remove('is-valid')
+            errores.classList.add('alert-warning')
+            errores.innerHTML = error
             validate.awards = false
-        }else{
+        } else {
             awards.classList.remove('is-invalid')
             awards.classList.add('is-valid')
             validate.awards = true
@@ -81,8 +104,10 @@ window.onload = function () {
         if (!release.value) {
             release.classList.add('is-invalid')
             release.classList.remove('is-valid')
+            errores.classList.add('alert-warning')
+            errores.innerHTML = error
             validate.release = false
-        }else{
+        } else {
             release.classList.remove('is-invalid')
             release.classList.add('is-valid')
             validate.release = true
@@ -93,8 +118,17 @@ window.onload = function () {
         if (!duracion.value) {
             duracion.classList.add('is-invalid')
             duracion.classList.remove('is-valid')
+            errores.classList.add('alert-warning')
+            errores.innerHTML = error
             validate.duracion = false
-        }else{
+        } else if (duracion.value <= 59 || duracion.value > 360) {
+            duracion.classList.add('is-invalid')
+            duracion.classList.remove('is-valid')
+            errores.classList.add('alert-warning')
+            errores.innerHTML = error
+            validate.duracion = false
+
+        } else {
             duracion.classList.remove('is-invalid')
             duracion.classList.add('is-valid')
             validate.duracion = true
@@ -105,8 +139,10 @@ window.onload = function () {
         if (!genre.value) {
             genre.classList.add('is-invalid')
             genre.classList.remove('is-valid')
+            errores.classList.add('alert-warning')
+            errores.innerHTML = error
             validate.genre = false
-        }else{
+        } else {
             genre.classList.remove('is-invalid')
             genre.classList.add('is-valid')
             validate.genre = true
@@ -126,3 +162,6 @@ window.onload = function () {
     }
     funcValidate(validate)
 }
+
+
+            /*  console.log('amigo esa peli no llega a 10 puntos'); */
